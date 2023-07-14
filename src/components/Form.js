3d4c90314b3b3ce073f0e6f0c8684fe59bbff459
jadewapi <125937514+jadewapi@users.noreply.github.com>
2023-07-14 18:18:07 -0500

    pass state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,28 +1,52 @@
 import { useState } from "react";
 
-export default function Form(e) {
+export default function Form() {
+  function formSubmit(e) {
+    e.preventDefault();
+
+    if (!itemDescription) return;
+
+    const newItem = {
+      itemDescription,
+      itemAmount,
+      packed: false,
+      id: Date.now(),
+    };
+    console.log(newItem);
+    setItemAmount(1);
+    setItemDescription("");
+    setAllItems((prevArray) => [...prevArray, newItem]);
+  }
   const [itemDescription, setItemDescription] = useState("");
+  const [itemAmount, setItemAmount] = useState(1);
+  const [allItems, setAllItems] = useState([]);
+
   return (
     <>
-      <div className="formContainer" onSubmit>
+      <div className="formContainer" onSubmit={formSubmit}>
         <form action="">
           <label>How many do you need for your trip?</label>
           <div className="inputContainer">
-            <select>
-              {}
-              <option value="1">1</option>
+            <select
+              value={itemAmount}
+              onChange={(e) => setItemAmount(e.target.value)}
+            >
+              {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+                <option value={num} key={num}>
+                  {num}
+                </option>
+              ))}
             </select>
             <input
               type="text"
               placeholder="item name"
               value={itemDescription}
-              onChange={() => setItemDescription((yeet) => e.target.value)}
+              onChange={(e) => setItemDescription(e.target.value)}
             />
             <button>add</button>
           </div>
         </form>
       </div>
-      n
     </>
   );
 }
